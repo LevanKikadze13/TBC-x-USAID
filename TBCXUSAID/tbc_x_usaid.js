9559208b00f041c7cb2db66df80b718c9777a7f4
LevanKikadze13 <146382647+LevanKikadze13@users.noreply.github.com>
2024-01-26 05:59:11 +0400

    sort of added slider

--- TBCXUSAID/tbc_x_usaid.js
+++ TBCXUSAID/tbc_x_usaid.js
@@ -1,3 +1,41 @@
+let currentSlide = 0;
+    let intervalId;
+    let nextBtn =document.querySelector("#nextBtn")
+    let prevBtn =document.querySelector("#prevBtn")
+
+    nextBtn.addEventListener('click', () =>{
+        nextSlide()
+    })
+    prevBtn.addEventListener('click', () =>{
+        prevSlide()
+    })
+    
+    const showSlide = index => {
+      const slider = document.querySelector('.slider');
+      const slides = document.querySelectorAll('.slide');
+      currentSlide = (index + slides.length) % slides.length;
+      slider.style.transform = `translateX(${-currentSlide * 100}%)`;
+    };
+  
+    const nextSlide = () => {
+      clearInterval(intervalId);
+      showSlide(currentSlide + 1);
+      startSlider();
+    };
+  
+    const prevSlide = () => {
+      clearInterval(intervalId);
+      showSlide(currentSlide - 1);
+      startSlider();
+    };
+  
+    // const startSlider = () => {
+    //   intervalId = setInterval(nextSlide, 3000);
+    // };
+  
+    // Initial start of the slider
+    startSlider();
+
 document.addEventListener("scroll", () => {
     const header = document.getElementById("js-header");
     if (window.scrollY > 0) {
