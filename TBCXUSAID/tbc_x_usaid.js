e3c1359b8f0da3f7c99b24d37bf6cf00dca55651
LevanKikadze13 <levanikokikadze@gmail.com>
2024-01-28 21:09:03 +0400

    finished

--- TBCXUSAID/tbc_x_usaid.js
+++ TBCXUSAID/tbc_x_usaid.js
@@ -1,3 +1,15 @@
+document.addEventListener("scroll", () => {
+  const header = document.getElementById("js-header");
+  if (window.scrollY > 0) {
+      header.style.backgroundColor = "rgb(26,30,31, 0.8)"; 
+  } else {
+      header.style.backgroundColor = "rgb(26,30,31)"; 
+  }
+});
+
+
+
+
 let currentSlideIndex = 1;
 let slideInterval;
 
